33e9df7ef7d642e3e1ebc8ccf2818db6832a1dbe
Sahand Raeisi <sahandraeisi1994@gmail.com>
2020-06-08 09:08:17 +0430

    Added CryptoCurrency protocol extensions example

--- where.playground/Contents.swift
+++ where.playground/Contents.swift
@@ -39,6 +39,8 @@ for number in numbers where number % 2 == 0 {
 
 // MARK: - Usage in protocol extensions
 
+/// EX-1
+
 extension Array where Element == Int {
     func printAverageAge() {
         let total = reduce(0, +)
@@ -47,8 +49,55 @@ extension Array where Element == Int {
     }
 }
 
+extension Dictionary where Value == Double {
+    func printShit() {
+        print("The value is Double")
+    }
+}
+
+
+let dict = ["Strudle" : 5.8]
+dict.printShit()
+
 let ages = [20, 26, 40, 60, 84]
 ages.printAverageAge()
+
+/// EX-2
+
+protocol CryptoCurrency {
+    var name: String { get }
+    var price: Double { get }
+    var amount: Decimal { get }
+}
+
+struct Bitcoin: CryptoCurrency {
+    let name = "Bitcoin"
+    let price: Double
+    let amount: Decimal
+}
+
+struct DogeCoin: CryptoCurrency {
+    let name = "DogeCoin"
+    let price: Double
+    let amount: Decimal
+}
+
+extension Array where Element == CryptoCurrency {
+    
+    var totalPrice: Double {
+        let total = self.map({$0.price}).reduce(0, +)
+        return total
+    }
+}
+
+let coins: [CryptoCurrency] = [
+    DogeCoin(price: 0.002150, amount: 300),
+    Bitcoin(price: 3661, amount: 1)
+]
+
+let formatter = NumberFormatter()
+formatter.minimumFractionDigits = 2
+formatter.string(from: NSNumber(value: coins.totalPrice)) // 3661,64
 
 // MARK: - Usage in first and contains
 
