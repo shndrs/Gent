44c2e4315b183d85b32244966f325423366216d0
Sahand Raeisi <sahandraeisi1994@gmail.com>
2020-06-08 10:36:53 +0430

    Added example number 2

--- Attributtes.playground/Contents.swift
+++ Attributtes.playground/Contents.swift
@@ -17,8 +17,46 @@ class Personn {
 /// but a return type, into a function with a parameter type of
 /// that original closure return type, so you can call it much more easy.
 
+/// EX-1
+
 func log(_ closure: @autoclosure () -> String) {
     print(closure())
 }
 
 log("shndrs")
+
+/// EX-2
+
+func goodMorning(morning: Bool, whom: @autoclosure () -> String) {
+    if morning {
+        print("Good morning, \(whom())")
+    }
+}
+
+func nameGenerator() -> String {
+    print("nameGenerator() is called")
+    return "Sahand"
+}
+
+goodMorning(morning: true, whom: nameGenerator())
+
+/// EX-2
+
+func complexFunction(clo: () -> String) {
+    print("Inside Function")
+    print(clo())
+}
+
+complexFunction { () -> String in
+    return "Nothing"
+}
+
+complexFunction(clo: { "Nothing" })
+// complexFunction(clo: "Nothing") - Will give you an Error because of this error we will use @autoclosure attribute
+
+func complexFunction2(clo: @autoclosure () -> String) {
+    print("Inside Function2")
+    print(clo())
+}
+
+complexFunction2(clo: "Nothing 2")
