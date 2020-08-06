df7a1630bd4d03aee25f316eb30b6223a508a3d0
shndrs <sahandraeisi1994@gmail.com>
2020-08-06 13:34:27 +0430

    Added UIViewController extension and StoryboardIds

---
+++ V-45/Vision45/Util/Extension/UIViewController.swift
@@ -0,0 +1,32 @@
+//
+//  UIViewController.swift
+//  Vision45
+//
+//  Created by Sahand Raeisi on 8/6/20.
+//  Copyright © 2020 Sahand Raeisi. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    public class func instantiate(from storyboard: UIStoryboard? = nil) -> Self {
+        return get(storyboard: storyboard)
+    }
+    
+    public class func instantiate(storyboard: StoryboardId) -> Self {
+        return instantiate(from: UIStoryboard(name: storyboard.rawValue, bundle: nil))
+    }
+       
+    @discardableResult
+    private class func get<T: UIViewController>(storyboard: UIStoryboard? = nil) -> T {
+        let className = String(describing: self)
+        var story = storyboard
+        if story == nil {
+            story = UIStoryboard(name: className, bundle: .main)
+        }
+        return story!.instantiateViewController(withIdentifier: className) as! T
+    }
+    
+}
+
---
+++ V-45/Vision45/Util/StoryboardId.swift
@@ -0,0 +1,16 @@
+//
+//  StoryboardId.swift
+//  Vision45
+//
+//  Created by Sahand Raeisi on 8/6/20.
+//  Copyright © 2020 Sahand Raeisi. All rights reserved.
+//
+
+import Foundation
+
+public enum StoryboardId: String {
+    
+    case main = "Main"
+    case login = "Login"
+    
+}
