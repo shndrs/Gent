ef5b86d86fcb07420e276e6f85e4ea6c650f28a8
shndrs <sahandraeisi1994@gmail.com>
2020-08-06 19:11:08 +0430

    Implemented LanguageSelectionView

--- V-45/Vision45/Module/LoginRegister/LanguageSelection/View/LanguageSelectionVC.swift
+++ V-45/Vision45/Module/LoginRegister/LanguageSelection/View/LanguageSelectionVC.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 final class LanguageSelectionVC: BaseViewController {
+    
+    private lazy var presenter: LanguageSelectionPresenter = {
+        return LanguageSelectionPresenter(view: self)
+    }()
 
     @IBOutlet private weak var persianButton: FirstInitialButton!
     @IBOutlet private weak var englishButton: FirstInitialButton!
@@ -20,15 +24,14 @@ final class LanguageSelectionVC: BaseViewController {
 fileprivate extension LanguageSelectionVC {
     
     @IBAction func persianButtonPressed(_ sender: FirstInitialButton) {
-        
+        presenter.setPersian()
     }
     
     @IBAction func englishButtonPressed(_ sender: FirstInitialButton) {
-        
+        presenter.setEnglish()
     }
     
 }
-
 
 // MARK: - Life Cycle
 
@@ -37,6 +40,20 @@ extension LanguageSelectionVC {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = Strings.languageSelection.value()
+    }
+    
+}
+
+// MARK: View Implementation
+
+extension LanguageSelectionVC: LanguageSelectionView {
+    
+    func goEnglish() {
+        
+    }
+    
+    func goPersian() {
+        
     }
     
 }
