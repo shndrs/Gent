340e5ba358927771cf3576947863bec09d2727a7
shndrs <sahandraeisi1994@gmail.com>
2020-08-06 11:38:37 +0430

    Added BaseViewController and LanguageSelectionVC

---
+++ V-45/Vision45/Module/Base/BaseViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BaseViewController.swift
+//  Vision45
+//
+//  Created by Sahand Raeisi on 8/6/20.
+//  Copyright © 2020 Sahand Raeisi. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ V-45/Vision45/Module/LanguageSelection/View/LanguageSelectionVC.swift
@@ -0,0 +1,38 @@
+//
+//  LanguageSelectionVC.swift
+//  Vision45
+//
+//  Created by Sahand Raeisi on 8/6/20.
+//  Copyright © 2020 Sahand Raeisi. All rights reserved.
+//
+
+import UIKit
+
+final class LanguageSelectionVC: BaseViewController {
+
+}
+
+// MARK: - Methods
+
+fileprivate extension LanguageSelectionVC {
+    
+    @IBAction func persianButtonPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func englishButtonPressed(_ sender: Any) {
+        
+    }
+    
+}
+
+
+// MARK: - Life Cycle
+
+extension LanguageSelectionVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
