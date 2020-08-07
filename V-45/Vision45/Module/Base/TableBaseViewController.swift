6c2c9142081c279b523d1cd2792e227d52cc0c75
shndrs <sahandraeisi1994@gmail.com>
2020-08-07 23:52:37 +0430

    Added TableBaseViewController

--- V-45/Vision45/Module/Base/TableBaseViewController.swift
+++ V-45/Vision45/Module/Base/TableBaseViewController.swift
@@ -6,4 +6,47 @@
 //  Copyright © 2020 Sahand Raeisi. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class TableBaseViewController: UIViewController {
+    
+    var largeTitle: Bool = false {
+        willSet(newVal) {
+            navigationController?.navigationBar.prefersLargeTitles = newVal
+        }
+    }
+    
+    @IBOutlet internal weak var tableView: UITableView! {
+        didSet {
+            tableSetup()
+        }
+    }
+    
+}
+
+// MARK: - Methods
+
+extension TableBaseViewController {
+    
+    @objc func tableSetup() {
+        tableView.cleanFooterView()
+    }
+    
+    func register(reuseIds: Array<String>) {
+        for id in reuseIds {
+            Register.in(component: tableView, id: id)
+        }
+    }
+    
+}
+
+// MARK: - Life Cycle
+
+extension TableBaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
