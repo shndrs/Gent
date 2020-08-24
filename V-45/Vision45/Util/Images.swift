39689f1c9ca7c65ba06d0327f323e17e509c9e9b
Sahand Raeisi <sahandraeisi1994@gmail.com>
2020-08-24 14:35:20 +0430

    Added Images

---
+++ V-45/Vision45/Util/Images.swift
@@ -0,0 +1,16 @@
+//
+//  Images.swift
+//  Vision45
+//
+//  Created by NP2 on 8/24/20.
+//  Copyright © 2020 Sahand Raeisi. All rights reserved.
+//
+
+import UIKit
+
+struct Images {
+    
+    static let lock = UIImage(named: "lock-4")!
+    static let unlock = UIImage(named: "unlock-3")!
+    
+}
