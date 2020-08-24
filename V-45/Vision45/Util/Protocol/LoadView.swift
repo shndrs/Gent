7dfe4af5db451956b7b1dc97ea8a073a8a732b5c
Sahand Raeisi <sahandraeisi1994@gmail.com>
2020-08-24 16:15:26 +0430

    Added LoadView

---
+++ V-45/Vision45/Util/Protocol/LoadView.swift
@@ -0,0 +1,14 @@
+//
+//  LoadView.swift
+//  Vision45
+//
+//  Created by NP2 on 8/24/20.
+//  Copyright © 2020 Sahand Raeisi. All rights reserved.
+//
+
+import Foundation
+
+protocol LoadView: BaseView {
+    func startLoading()
+    func stopLoading()
+}
