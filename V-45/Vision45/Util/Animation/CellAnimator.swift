7cc2e0f99b417c8a40fbcc0e5725b592e4ca6bda
Sahand Raeisi <sahandraeisi1994@gmail.com>
2020-08-25 08:40:29 +0430

    Added CellAnimator

---
+++ V-45/Vision45/Util/Animation/CellAnimator.swift
@@ -0,0 +1,44 @@
+//
+//  CellAnimator.swift
+//  Vision45
+//
+//  Created by NP2 on 8/25/20.
+//  Copyright © 2020 Sahand Raeisi. All rights reserved.
+//
+
+import UIKit
+
+final class CellAnimator: NSObject {
+    
+    private var cell: UITableViewCell?
+    
+    init(cell: UITableViewCell) {
+        self.cell = cell
+    }
+    
+    public func fadeAnimation() {
+        
+        cell?.alpha = 0
+        UIView.animate(withDuration: 0.2) { [weak self] in
+            self?.cell?.alpha = 1
+        }
+    }
+    
+    public func zoomIn() {
+        cell?.alpha = 0
+        cell?.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
+        UIView.animate(withDuration: 1.3, delay: 0,
+                       options: .curveEaseIn, animations: { [weak self] in
+            self?.cell?.transform = CGAffineTransform.identity
+            self?.cell?.alpha = 1
+        })
+    }
+    
+    public func sweepIn() {
+        cell?.transform = CGAffineTransform(translationX: -UIScreen.main.bounds.width, y: 0)
+        UIView.animate(withDuration: 0.9, delay: 0,
+                       options: .curveEaseInOut, animations: { [weak self] in
+            self?.cell?.transform = .identity
+        })
+    }
+}
