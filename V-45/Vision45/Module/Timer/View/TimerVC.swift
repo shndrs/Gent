481c47e16f52aa3e8cd6e01c16b2d6255e4c9850
Sahand Raeisi <sahandraeisi1994@gmail.com>
2020-09-08 10:08:32 +0430

    Added BaseDatePicker and BaseSwitch

---
+++ V-45/Vision45/Module/Timer/View/TimerVC.swift
@@ -0,0 +1,30 @@
+//
+//  TimerVC.swift
+//  Vision45
+//
+//  Created by NP2 on 9/8/20.
+//  Copyright © 2020 Sahand Raeisi. All rights reserved.
+//
+
+import UIKit
+
+class TimerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
