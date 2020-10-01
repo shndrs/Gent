2e6df63477bea813c8124da7988f32587ff99389
shndrs <sahandraeisi1994@gmail.com>
2020-10-01 19:33:54 +0330

    Updated Home.storyboard with MenuVC

--- V-45/Vision45/Module/Home/View/HomeTBC.swift
+++ V-45/Vision45/Module/Home/View/HomeTBC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-final class HomeTBC: BaseViewController {
+final class HomeTBC: UITabBarController {
     
     
     
--- V-45/Vision45/Module/Menu/Model/Menu.swift
+++ V-45/Vision45/Module/Menu/Model/Menu.swift
@@ -23,4 +23,3 @@ final class Menu: NSObject {
     }
     
 }
-
