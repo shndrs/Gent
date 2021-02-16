51dd12e2a1ef29ef5bb686a6b394614461ae3afc
Sahand Raeisi <sahandraeisi1994@gmail.com>
2021-02-16 11:02:15 +0330

    Added Calendar cells

---
+++ CalendarView/CalendarView/Cell/CalendarCell.swift
@@ -0,0 +1,16 @@
+//
+//  CalendarCell.swift
+//  CalendarView
+//
+//  Created by shndrs on 2/16/21.
+//
+
+import JTAppleCalendar
+
+final class CalendarCell: JTACDayCell {
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var selectedView: UIView!
+    
+}
+
---
+++ CalendarView/CalendarView/Cell/CalendarDayCell.swift
@@ -0,0 +1,20 @@
+//
+//  CalendarDayCell.swift
+//  CalendarView
+//
+//  Created by shndrs on 2/16/21.
+//
+
+import UIKit
+import JTAppleCalendar
+
+final class CalendarDayCell: JTACDayCell {
+
+    @IBOutlet var selectedView: UIView!
+    @IBOutlet var dayLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
