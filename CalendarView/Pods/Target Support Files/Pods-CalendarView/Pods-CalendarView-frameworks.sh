b29f9d788c8bd115127e19192d73abaaa443bbd9
Sahand Raeisi <sahandraeisi1994@gmail.com>
2021-02-21 08:12:53 +0330

    Added KDCalendar

--- CalendarView/Pods/Target Support Files/Pods-CalendarView/Pods-CalendarView-frameworks.sh
+++ CalendarView/Pods/Target Support Files/Pods-CalendarView/Pods-CalendarView-frameworks.sh
@@ -176,9 +176,11 @@ code_sign_if_enabled() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/JTAppleCalendar/JTAppleCalendar.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/KDCalendar/KDCalendar.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/JTAppleCalendar/JTAppleCalendar.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/KDCalendar/KDCalendar.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
