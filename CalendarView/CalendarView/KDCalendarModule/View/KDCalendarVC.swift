68bd60f17b95229ff4e9a2925021208abfe57d8a
Sahand Raeisi <sahandraeisi1994@gmail.com>
2021-02-21 08:17:15 +0330

    Added KDCalendarVC

---
+++ CalendarView/CalendarView/KDCalendarModule/View/KDCalendarVC.swift
@@ -0,0 +1,24 @@
+//
+//  KDCalendarVC.swift
+//  CalendarView
+//
+//  Created by shndrs on 2/21/21.
+//
+
+import UIKit
+
+final class KDCalendarVC: UIViewController {
+
+    
+    
+}
+
+// MARK: - Life Cycle
+
+extension KDCalendarVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
