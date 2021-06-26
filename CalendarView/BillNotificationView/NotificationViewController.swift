9f0fce3ec172c7e53bbf764efa499bd4e065c852
Sahand Raeisi <sahandraeisi1994@gmail.com>
2021-06-26 10:32:13 +0430

    Updated notification interface

--- CalendarView/BillNotificationView/NotificationViewController.swift
+++ CalendarView/BillNotificationView/NotificationViewController.swift
@@ -11,6 +11,13 @@ import UserNotificationsUI
 
 class NotificationViewController: UIViewController, UNNotificationContentExtension {
 
+    @IBOutlet private var lblTitle: UILabel!
+    @IBOutlet private var lblContent: UILabel!
+    @IBOutlet private var imgBill: UIImageView! {
+        didSet {
+            imgBill.image = #imageLiteral(resourceName: "credit-card-suggest")
+        }
+    }
     @IBOutlet var label: UILabel?
     
     override func viewDidLoad() {
